73d958f3f616044ee042ec8b875396e5763f533b
irene <i.tomaini@tuotempo.com>
2020-07-17 14:05:58 +0200

    calculator finished

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Calculator from './Calculator';
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(<Calculator />, document.getElementById('main'));
+});
